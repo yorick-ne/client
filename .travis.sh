e867bbdab57e43f4c06692efc84fd67508519878
Eximius <eximius@gmx.com>
2014-10-25 06:06:50 +0100

    Un-derp

--- .travis.sh
+++ .travis.sh
@@ -3,7 +3,7 @@
 # Install libgit2 from untrusted source ppa
 sudo add-apt-repository ppa:xav0989/libgit2 -y
 
-sudo apt-get update -q > /dev/null && echo "apt-get caches updated."
+sudo apt-get update -q &> /dev/null && echo "apt-get caches updated."
 
 if [[ $? != 0 ]]; then
 	echo "apt-get update failed." > /dev/fd/2
@@ -12,5 +12,5 @@ fi
 
 pip install -q cffi && echo "Installed cffi."
 sudo apt-get install -q libgit2-dev python-qt4 > /dev/null \
-	&& "Installed libgit2-dev python-qt4"
+	&& echo "Installed libgit2-dev python-qt4"
 
