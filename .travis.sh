0ef3823c6f18d7cdd727dbb4d2d331509e48a538
Eximius <eximius@gmx.com>
2014-10-25 06:03:05 +0100

    More crap

--- .travis.sh
+++ .travis.sh
@@ -3,15 +3,14 @@
 # Install libgit2 from untrusted source ppa
 sudo add-apt-repository ppa:xav0989/libgit2 -y
 
-sudo apt-get update -q > /dev/null && echo "apt-get caches updated"
+sudo apt-get update -q > /dev/null && echo "apt-get caches updated."
 
 if [[ $? != 0 ]]; then
 	echo "apt-get update failed." > /dev/fd/2
 	exit 1
 fi
 
-
-sudo apt-get install -q cmake && echo "Installed cmake."
 pip install -q cffi && echo "Installed cffi."
-sudo apt-get install -q libgit2-dev python-qt4
+sudo apt-get install -q libgit2-dev python-qt4 > /dev/null \
+	&& "Installed libgit2-dev python-qt4"
 
