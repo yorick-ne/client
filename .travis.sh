6199ac0ddc52784e80d119924860d7c0c33cc447
Eximius <eximius@gmx.com>
2014-10-25 07:42:07 +0100

    Life is beautiful. Everything's awesome.

--- .travis.sh
+++ .travis.sh
@@ -1,31 +1,13 @@
 #!/bin/sh
 
-# Install libgit2 from untrusted source ppa
-# sudo add-apt-repository "deb http://ubuntu-archive.mirror.nucleus.be/ utopic main"
-# sudo add-apt-repository "deb-src http://ubuntu-archive.mirror.nucleus.be/ utopic main"
+# Install libgit2 from a bunch of Ubuntu 15.04 binary repos
+mkdir libgit2
+pushd libgit2
 
-# sudo apt-get update -q &> /dev/null && echo "apt-get caches updated."
-
-# if [[ $? != 0 ]]; then
-# 	echo "apt-get update failed." > /dev/fd/2
-# 	exit 1
-# fi
-
-pushd
-
-mkdir banana
-cd banana
-
-wget "https://launchpad.net/ubuntu/+source/libgit2/0.21.1-1/+build/6494190/+files/libgit2-21_0.21.1-1_amd64.deb"
-wget "https://launchpad.net/ubuntu/+source/libgit2/0.21.1-1/+build/6494190/+files/libgit2-dev_0.21.1-1_amd64.deb"
+wget -q --progress=bar "https://launchpad.net/ubuntu/+source/libgit2/0.21.1-1/+build/6494190/+files/libgit2-21_0.21.1-1_amd64.deb"
+wget -q --progress=bar "https://launchpad.net/ubuntu/+source/libgit2/0.21.1-1/+build/6494190/+files/libgit2-dev_0.21.1-1_amd64.deb"
 
 sudo dpkg -i "libgit2-21_0.21.1-1_amd64.deb"
 sudo dpkg -i "libgit2-dev_0.21.1-1_amd64.deb"
 
-pip install -q cffi && echo "Installed cffi."
-sudo apt-get install -q python-qt4 > /dev/null \
-	&& echo "Installed python-qt4" \
-	|| exit 1
-
-
-dpkg -s cffi libgit2-dev python-qt4 | grep 'Version'
+popd
