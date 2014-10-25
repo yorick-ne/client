63b788dce46b54838ee359cfc826e2da455d7f8a
Eximius <eximius@gmx.com>
2014-10-25 05:38:06 +0100

    Less crap

--- .travis.sh
+++ .travis.sh
@@ -1,25 +1,20 @@
 #!/bin/sh
 
-sudo apt-get install cmake
-pip install cffi
-
-pushd
-
-cd ~
-
 # Install libgit2 from ubuntu vivid repos
 apt_repo="
 deb http://archive.canonical.com/ubuntu vivid banana
-deb-src http://archive.canonical.com/ubuntu vivid banana"
+deb-src http://archive.canonical.com/ubuntu vivid banana
+"
 sudo echo $apt_repo >> "/etc/apt/sources.list"
 
-sudo apt-get update
+sudo apt-get update -q && echo "apt-get caches updated"
 
 if [[ $? != 0 ]]; then
 	echo "apt-get update failed." > /dev/fd/2
 	exit 1
 fi
 
-sudo apt-get install libgit2-dev
 
-sudo apt-get install python-qt4
+sudo apt-get install -q cmake && echo "Installed cmake."
+pip install -q cffi && echo "Installed cffi."
+sudo apt-get install -q libgit2-dev python-qt4
